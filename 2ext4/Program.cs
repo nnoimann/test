8149fcfada5db52c39c6a18bd6f8947805a6f478
Nazar Noimann <noimannnazar@gmail.com>
2022-07-11 21:29:02 +0300

    v1

--- 2ext4/Program.cs
+++ 2ext4/Program.cs
@@ -10,7 +10,25 @@ namespace _2ext4
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Enter the first number:");
+            int num_first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int num_second = Convert.ToInt32(Console.ReadLine());
+            int Quotient_of_division = num_first % num_second;
+            int Result = num_first / num_second;
+            if (Quotient_of_division == 0)
+            {
+                Console.WriteLine("The number to divide without a remainder");
+                Console.WriteLine("Result " + Result);
+            } else if(Quotient_of_division != 0)
+            {
+                Console.WriteLine("The number is divisible with the remainder");
+                Console.WriteLine("Result" + Result);
+                Console.WriteLine("Quotient of division " + Quotient_of_division);
+            } else if (num_second == 0)
+            {
+                Console.WriteLine("Infinity");
+            }
         }
     }
 }
